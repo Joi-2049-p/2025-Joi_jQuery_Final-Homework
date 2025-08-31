ba237db959d0166db05e658069e6edb685085de2
Joi-2049-p <cadeus12@gmail.com>
2025-08-31 20:47:22 +0800

    2025-Joi_jQuery_Final Homework

---
+++ JS/all.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  // ============ navbar toggle ==================//
+  $("nav li a").click(function (event) {
+    event.preventDefault();
+    $(this).toggleClass("active");
+  });
+
+  $(".nav-bnt-1").click(function (event) {
+    event.preventDefault();
+    $(".item_1").slideToggle();
+  });
+
+  $(".nav-bnt-2").click(function (event) {
+    event.preventDefault();
+    $(".item_2").slideToggle();
+  });
+
+  $(".nav-bnt-3").click(function (event) {
+    event.preventDefault();
+    $(".item_3").slideToggle();
+  });
+
+  // ============ offcanvas ==================//
+  $(".hamburger-bnt").click(function (event) {
+    event.preventDefault();
+    $(".side_menu").toggleClass("menu_open");
+  });
+
+  $(".menu-close").click(function (event) {
+    event.preventDefault();
+    $(".side_menu").toggleClass("menu_open");
+  });
+
+  // ============ ScrollTop==================//
+  $(".scroll_bnt a").click(function (event) {
+    event.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+
+  // ============ Swiper ==================//
+
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    speed: 3000,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    effect: "slide",
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // ============ lightbox ==================//
+
+  lightbox.option({
+    resizeDuration: 500,
+    wrapAround: true,
+  });
+});
